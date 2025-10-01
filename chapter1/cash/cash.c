f7897e200c3f5f74ade3d088f51645a82cace437
bpricemg <43038187+bpricemg@users.noreply.github.com>
2025-10-01 15:14:37 -0400

    Updated 10/1

---
+++ chapter1/cash/cash.c
@@ -0,0 +1,39 @@
+/**
+ * Cash
+ *
+ * CS50 AP
+ * Name
+ * Date
+ *
+ */
+
+ #include <stdio.h>
+
+ int main(void)
+ {
+    // Get the change from user, be sure the user doesn't provide a negative
+
+    do
+    {
+
+    }
+    while()
+
+    // Initialize a coins counter to 0
+
+    // Determine the number of quarters needed
+    while(change >= .25)
+    {
+        // Subtract .25 from change
+
+        // Update the coins counter by 1
+    }
+
+    // Determine the number of dimes
+
+    // Determine the number of nickles
+
+    // Determine the number pennies
+
+    // Print result
+ }
