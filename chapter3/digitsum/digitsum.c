8f8a60cd236c2d9bd9b0694e7421fefa5ea15023
bpricemg <bprice@mgrsd.org>
2025-12-16 14:17:52 -0500

    Updated 12/16

---
+++ chapter3/digitsum/digitsum.c
@@ -0,0 +1,27 @@
+// DigitSum
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int digitsum(int n);
+
+int main(void)
+{
+    // Get an integer from a user
+    int n = get_int("Integer: ");
+
+    // Print result with a function call of digitsum()
+    printf("Sum of the digits: %d\n", digitsum(n));
+}
+
+// TODO: Recursive implementation for adding each digit of an integer
+int digitsum(int n)
+{
+    // Base case
+    if (???)
+        return ???;
+        
+    // Recursive case
+    else
+        return ???;
+}
