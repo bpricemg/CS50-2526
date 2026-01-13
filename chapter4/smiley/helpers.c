69cc9187092f939377d2d5482db4695150f38304
bpricemg <43038187+bpricemg@users.noreply.github.com>
2026-01-13 14:17:11 -0500

    Updated 1/13

--- chapter4/smiley/helpers.c
+++ chapter4/smiley/helpers.c
@@ -10,7 +10,23 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j< width; j++)
         {
             // Change all black pixels to bright yellow
-            if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00
+            if (image[i][j].rgbtRed == 0x00 &&
+                image[i][j].rgbtGreen == 0x00 &&
+                image[i][j].rgbtBlue == 0x00)
+            {
+                image[i][j].rgbtRed = 0xff;
+                image[i][j].rgbtGreen = 0xff;
+            }
+
+            // Change all white pixels to pink
+            if (image[i][j].rgbtRed == 255 &&
+                image[i][j].rgbtGreen == 255 &&
+                image[i][j].rgbtBlue == 255)
+            {
+                image[i][j].rgbtRed = 0xc0;
+                image[i][j].rgbtGreen = 0x40;
+                image[i][j].rgbtBlue = 0x98;
+            }
         }
     }
 }
