7b1c7bac7e7f88428827c10585321f00022cf1ee
bpricemg <43038187+bpricemg@users.noreply.github.com>
2026-01-22 10:01:29 -0500

    Updated 1/22

--- chapter4/filter-less/helpers.c
+++ chapter4/filter-less/helpers.c
@@ -55,5 +55,40 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Create a copy of image
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
+
+    // Loop through the original color values located in copy[]
+     for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Loop through the "9" surrounding pixels
+            for (int row  = i - 1; row <= i + 1; row++)
+            {
+                for (int col = ????)
+                {
+                    // IF that the pixel is actually on the pixel array
+                    // (make sure row and col aren't negative and row not bigger than
+                    // width - 1 and col not bigger than height - 1)
+                    if ()
+                    {
+                        // Update a count for the denominator of the average
+
+                        // Add the original red, green, blue values to a running sum
+                    }
+                }
+            }
+
+            // Update the new pixel value in image[i][j]
+        }
+    }
     return;
 }
