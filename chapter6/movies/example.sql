503c9c4d5b110bcf3dba0cf48734a657769bdb97
bpricemg <43038187+bpricemg@users.noreply.github.com>
2026-04-09 09:48:45 -0400

    Delete chapter6 directory

--- chapter6/movies/example.sql
+++
@@ -1,4 +0,0 @@
--- List all the stars in Harry Potter and the Sorcerer's Stone
-SELECT name FROM people WHERE id IN
-(SELECT person_id FROM stars WHERE movie_id =
-(SELECT id FROM movies WHERE title LIKE '%Sorcerer''s Stone%'));
